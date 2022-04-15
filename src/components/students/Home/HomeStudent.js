739c42ad525cbec131c75450640414caa8d7881f
houdini-21 <marinerocruzfer@gmail.com>
2022-04-15 06:08:10 -0600

    search working

--- src/components/students/Home/HomeStudent.js
+++ src/components/students/Home/HomeStudent.js
@@ -2,30 +2,78 @@ import React, { useEffect, useState, useContext } from "react";
 import httpClient from "../../../services/services";
 import { AuthContext } from "../../../auth/AuthContext";
 import Cards from "../UI/Cards";
+import { useFormik } from "formik";
 import { ToastContainer } from "react-toastify";
 import { loadProgressBar } from "axios-progress-bar";
 
 const HomeStudent = () => {
   const [books, setBooks] = useState([]);
   const [page, setPage] = useState(1);
+  const [body, setBody] = useState({});
   const { user } = useContext(AuthContext);
 
-  useEffect(() => {
-    const body = {};
+  const Formik = useFormik({
+    initialValues: {
+      title: "",
+      publishedYear: "",
+      genre: "",
+      author: "",
+    },
+    onSubmit: (values) => {
+      setBody(values);
+      setPage(1);
+      setBooks([]);
+    },
+  });
 
-    httpClient
-      .post(`student/books/search/${page}`, body, {
+  const loadBooks = async () => {
+    const { data } = await httpClient.post(
+      `student/books/search/${page}`,
+      body,
+      {
         Authorization: `JWT ${user.token}`,
-      })
-      .then((res) => {
-        setBooks((books) => books.concat(res.data.books));
-        loadProgressBar();
-      });
-    console.log(books);
-  }, [page]);
+      }
+    );
+    setBooks((books) => books.concat(data.books));
+    loadProgressBar();
+  };
+
+  useEffect(() => {
+    loadBooks();
+  }, [page, body]);
 
   return (
     <div className="px-8 py-5 w-full">
+      <div className="flex flex-col items-center">
+        <form onSubmit={Formik.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={Formik.handleChange}
+          />
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            onChange={Formik.handleChange}
+          />
+          <input
+            type="text"
+            name="publishedYear"
+            placeholder="Published Year"
+            onChange={Formik.handleChange}
+          />
+          <input
+            type="text"
+            name="genre"
+            placeholder="Genre"
+            onChange={Formik.handleChange}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
       <div className="flex flex-row w-full flex-wrap justify-around">
         {books.map((book) => (
           <Cards
