d1a760d1b6efdcb7ff17ad4415dd357fbe9c5535
houdini-21 <marinerocruzfer@gmail.com>
2022-04-15 09:49:39 -0600

    now users can view the history of requested books

--- src/components/students/Requests/RequestStudent.js
+++ src/components/students/Requests/RequestStudent.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import httpClient from "../../../services/services";
+import { AuthContext } from "../../../auth/AuthContext";
+import { loadProgressBar } from "axios-progress-bar";
+import { toast } from "react-toastify";
+import CardItem from "../UI/CardItem";
 
 const RequestStudent = () => {
-  return <h2>RequestStudent</h2>;
+  const { user } = useContext(AuthContext);
+  const [page, setPage] = useState(1);
+  const [books, setBooks] = useState([]);
+
+  const requestBookingHistory = () => {
+    const body = {
+      idStudent: user.userId,
+    };
+    httpClient
+      .post(`student/books/myrequests/${page}`, body, {
+        Authorization: `JWT ${user.token}`,
+      })
+      .then((res) => {
+        setBooks((books) => books.concat(res.data.books));
+        loadProgressBar();
+      });
+  };
+
+  useEffect(() => {
+    requestBookingHistory();
+  }, [page]);
+
+  return (
+    <div className="px-8 py-5 w-full">
+      <div className="flex flex-row w-full flex-wrap justify-around mt-4 mb-4">
+        {books.map((book) => (
+          <CardItem
+            key={book._id}
+            idBook={book.idBook._id}
+            title={book.idBook.title}
+            author={book.idBook.author}
+            publishedYear={book.idBook.publishedYear}
+            genre={book.idBook.genre}
+            dateRequest={book.dateRequest}
+            status={book.status}
+            dateReturn={book.dateReturn}
+          />
+        ))}
+      </div>
+      <button
+        className="border-blue-500 hover:bg-blue-500 hover:text-white text-blue-500 border-2 text-lg font-bold py-2 px-4 rounded w-full h-12 mt-4"
+        onClick={() => setPage(page + 1)}
+      >
+        Load more
+      </button>
+    </div>
+  );
 };
 
 export default RequestStudent;
--- src/components/students/UI/Cards.js
+++ src/components/students/UI/Cards.js
@@ -3,7 +3,7 @@ import httpClient from "../../../services/services";
 import { AuthContext } from "../../../auth/AuthContext";
 import { loadProgressBar } from "axios-progress-bar";
 import { toast } from "react-toastify";
-
+import CardItem from "./CardItem";
 
 const Cards = ({ idBook, title, author, publishedYear, genre, stock }) => {
   const { user } = useContext(AuthContext);
@@ -11,8 +11,9 @@ const Cards = ({ idBook, title, author, publishedYear, genre, stock }) => {
   const requestBooking = (idBook) => {
     const body = {
       idBook,
-      idStudent: user.id,
+      idStudent: user.userId,
     };
+    console.log(body);
     httpClient
       .post(`student/books/requestedBook`, body, {
         Authorization: `JWT ${user.token}`,
@@ -33,75 +34,15 @@ const Cards = ({ idBook, title, author, publishedYear, genre, stock }) => {
 
   return (
     <>
-      <div
-        className="
-          sm:h-56
-          w-full
-          md:w-1/2 lg:w-1/3 xl:w-1/3
-        "
-      >
-        <div
-          className="
-          bg-white
-          shadow-md
-          h-96
-          rounded-3xl
-          flex flex-col
-          justify-center
-          items-center
-          overflow-hidden
-          sm:flex-row 
-          w-11/12 sm:h-56
-          "
-        >
-          <img
-            className="h-1/2 w-full sm:h-full sm:w-1/2 object-cover"
-            src="https://images.unsplash.com/photo-1484101403633-562f891dc89a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80"
-            alt="image"
-          />
-
-          <div
-            className="
-            flex-1
-            w-full
-            flex flex-col
-            items-baseline
-            justify-around
-            h-1/2
-        
-            px-4
-            sm:h-full sm:items-baseline sm:w-1/2
-          "
-          >
-            <div className="flex flex-col justify-start items-baseline">
-              <h1 className="text-lg font-normal mb-0 text-gray-600 font-sans">
-                {title} - {publishedYear}
-              </h1>
-              <p className="text-sm text-indigo-300 mt-0 font-bold">
-                Author: <span className="font-normal">{author}</span>
-              </p>
-              <p className="text-sm text-indigo-300 mt-0 font-bold">
-                Genre: <span className="font-normal">{genre}</span>
-              </p>
-            </div>
-            <div className="w-full flex justify-between items-start flex-col">
-              <h3 className="font-bold text-gray-500">
-                Stock:{" "}
-                <span className="font-normal">
-                  {stock == 0 ? "Out Stock" : stock}
-                </span>
-              </h3>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white cursor-pointer font-bold py-2 px-4 rounded mt-3.5 w-full disabled:bg-blue-300 disabled:hover:bg-blue-400 disabled:cursor-not-allowed"
-                disabled={stock == 0}
-                onClick={() => requestBooking(idBook)}
-              >
-                Rent
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CardItem
+        idBook={idBook}
+        title={title}
+        author={author}
+        publishedYear={publishedYear}
+        genre={genre}
+        stock={stock}
+        requestBooking={requestBooking}
+      />
     </>
   );
 };
